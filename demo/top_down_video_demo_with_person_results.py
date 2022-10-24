6e35218cd5619bc80fec4b1fb90ee33babf114d3
wheemyungshin <zerger26@gmail.com>
2022-10-24 17:38:46 +0900

    demo with json results

--- demo/top_down_video_demo_with_person_results.py
+++ demo/top_down_video_demo_with_person_results.py
@@ -7,16 +7,40 @@ import cv2
 import mmcv
 
 from mmpose.apis import (collect_multi_frames, inference_top_down_pose_model,
-                         init_pose_model, process_mmdet_results,
+                         init_pose_model, 
                          vis_pose_result)
 from mmpose.datasets import DatasetInfo
 
-try:
-    from mmdet.apis import inference_detector, init_detector
-    has_mmdet = True
-except (ImportError, ModuleNotFoundError):
-    has_mmdet = False
+import numpy as np
+import json
 
+from collections import defaultdict
+
+def load_person_detection_results(args):
+    """Load coco person detection results."""
+    num_joints = 17
+    all_boxes = None
+    with open(args.person_results, 'r') as f:
+        all_boxes = json.load(f)
+
+    if not all_boxes:
+        raise ValueError('=> Load %s fail!' % args.person_results)
+
+    print(f'=> Total boxes: {len(all_boxes)}')
+    
+    person_results = defaultdict(list)
+    for det_res in all_boxes:
+        if det_res['category_id'] != 1:
+            continue
+
+        box = det_res['bbox']
+        score = det_res['score']
+        box_5 = [box[0], box[1], box[2], box[3], score]
+
+        person = {}
+        person['bbox'] = box_5
+        person_results[det_res['video_path']].append((person, det_res['image_id']))
+    return person_results
 
 def main():
     """Visualize the demo video (support both single-frame and multi-frame).
@@ -24,8 +48,7 @@ def main():
     Using mmdet to detect the human.
     """
     parser = ArgumentParser()
-    parser.add_argument('det_config', help='Config file for detection')
-    parser.add_argument('det_checkpoint', help='Checkpoint file for detection')
+    parser.add_argument('person_results', help='Json file for detection')
     parser.add_argument('pose_config', help='Config file for pose')
     parser.add_argument('pose_checkpoint', help='Checkpoint file for pose')
     parser.add_argument('--video-path', type=str, help='Video path (video file or dir)')
@@ -52,7 +75,17 @@ def main():
         default=0.3,
         help='Bounding box score threshold')
     parser.add_argument(
+        '--min-bbox-size',
+        type=float,
+        default=0,
+        help='Bounding box size threshold')
+    parser.add_argument(
         '--kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
+    parser.add_argument(
+        '--min-points',
+        type=int,
+        default=0,
+        help='Minimum predicted pose joints threshold')
     parser.add_argument(
         '--radius',
         type=int,
@@ -77,20 +110,20 @@ def main():
         help='inference mode. If set to True, can not use future frame'
         'information when using multi frames for inference in the pose'
         'estimation stage. Default: False.')
-
-    assert has_mmdet, 'Please install mmdet to run the demo.'
+    parser.add_argument(
+        '--resize-w',
+        type=int,
+        default=0)
+    parser.add_argument(
+        '--resize-h',
+        type=int,
+        default=0)
 
     args = parser.parse_args()
 
     assert args.show or (args.out_video_root != '')
-    assert args.det_config is not None
-    assert args.det_checkpoint is not None
 
     print('Initializing model...')
-    # build the detection model from a config file and a checkpoint file
-    det_model = init_detector(
-        args.det_config, args.det_checkpoint, device=args.device.lower())
-
     # build the pose model from a config file and a checkpoint file
     pose_model = init_pose_model(
         args.pose_config, args.pose_checkpoint, device=args.device.lower())
@@ -116,6 +149,9 @@ def main():
     else:
         video_path_list = [args.video_path]
 
+    #read person results
+    person_results_dict = load_person_detection_results(args)
+
     # read video
     for video_path in video_path_list:
         video = mmcv.VideoReader(video_path)
@@ -129,13 +165,18 @@ def main():
 
         if save_out_video:
             fps = video.fps
-            size = (video.width, video.height)
+            if args.resize_h == 0 or args.resize_w == 0:
+                size = (video.width, video.height)
+            else:
+                size = (args.resize_w, args.resize_h)
+            
             print("SIZE: ", size)
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
             videoWriter = cv2.VideoWriter(
                 os.path.join(args.out_video_root,
                             f'vis_{os.path.basename(video_path)}'), fourcc,
                 fps, size)
+        print("Loading...:", video_path)
 
         # frame index offsets for inference, used in multi-frame inference setting
         if args.use_multi_frames:
@@ -149,24 +190,28 @@ def main():
         # e.g. use ('backbone', ) to return backbone feature
         output_layer_names = None
 
+
         print('Running inference...')
         for frame_id, cur_frame in enumerate(mmcv.track_iter_progress(video)):
-            # get the detection results of current frame
-            # the resulting box is (x1, y1, x2, y2)
-            mmdet_results = inference_detector(det_model, cur_frame)
+            if not (args.resize_h == 0 or args.resize_w == 0):
+                cur_frame = cv2.resize(cur_frame, (args.resize_w, args.resize_h), interpolation=cv2.INTER_AREA)
 
-            # keep the person class bounding boxes.
-            person_results = process_mmdet_results(mmdet_results, args.det_cat_id)
+            person_results = person_results_dict[video_path.split("/")[-1]]
 
-            if args.use_multi_frames:
-                frames = collect_multi_frames(video, frame_id, indices,
-                                            args.online)
+            filtered_person_results = []
+            for person_result, person_image_id in person_results:
+                if person_image_id == frame_id:
+                    #print("person_result: ", person_result)
+                    #print("person_image_id: ", person_image_id)
+                    person_size = (person_result['bbox'][2]-person_result['bbox'][0])*(person_result['bbox'][3]-person_result['bbox'][1])
+                    if person_size >= args.min_bbox_size:
+                        filtered_person_results.append(person_result)
 
             # test a single image, with a list of bboxes.
             pose_results, returned_outputs = inference_top_down_pose_model(
                 pose_model,
                 frames if args.use_multi_frames else cur_frame,
-                person_results,
+                filtered_person_results,
                 bbox_thr=args.bbox_thr,
                 format='xyxy',
                 dataset=dataset,
@@ -174,11 +219,16 @@ def main():
                 return_heatmap=return_heatmap,
                 outputs=output_layer_names)
 
+            new_pose_results = []
+            for pose_result in pose_results:
+                if np.sum(pose_result['keypoints'][:,-1] > args.kpt_thr) >= args.min_points:
+                    new_pose_results.append(pose_result)
+
             # show the results
             vis_frame = vis_pose_result(
                 pose_model,
                 cur_frame,
-                pose_results,
+                new_pose_results,
                 dataset=dataset,
                 dataset_info=dataset_info,
                 kpt_score_thr=args.kpt_thr,
